952fdb3f5041c156ed0427455688dc9306e63dd7
RDAxRoadkill <wouter97@planet.nl>
2019-05-23 16:23:20 +0200

    Finished assignment, added really basic maze

--- GuardedMaze/Assets/Scripts/GuardController.cs
+++ GuardedMaze/Assets/Scripts/GuardController.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GuardController : MonoBehaviour
 {
     private Rigidbody rb;
     public Text winText;
     public float speed;
-    public float min = 2f;
-    public float max = 3f;
+    public float min;
+    public float max;
 
     // Start is called before the first frame update
     void Start()
@@ -22,25 +23,39 @@ public class GuardController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, max - min) + min, transform.position.y, transform.position.z);
+        if(gameObject.tag == "VerticalGuard")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * speed, max - min) + min);
+        }
+        //else if (gameObject.tag == "Gate")
+        //{
+        //    transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, max - min) + min, transform.position.z);
+        //}
+        else
+        {
+            transform.position = new Vector3(Mathf.PingPong(Time.time * speed, max - min) + min, transform.position.y, transform.position.z);
+        }
+        
     }
 
     void FixedUpdate()
     {
         rb.velocity = transform.TransformDirection(Vector3.forward * speed);
     }
-
+    //Probably should move this to a seperate function. Instead of including this to every guard
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.SetActive(false);
             SetFailText();
+           // Application.LoadLevel(Application.loadedLevel);
+            
         }
     }
-
+    //Probably should move this to a seperate function. Instead of including this to every guard
     void SetFailText()
     {
-        winText.text = "You Lost";
+        winText.text = "You Lost!";
     }
 }

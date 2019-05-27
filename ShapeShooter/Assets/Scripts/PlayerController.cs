2d2b924d6034b3ac678067bb0dcdf0a85e619012
Wouter Jansen <wouter97@planet.nl>
2019-05-27 14:51:11 +0200

    First Functional version of game

---
+++ ShapeShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject circle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Optimize this
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Instantiate(square, transform.position, Quaternion.identity);
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Instantiate(triangle, transform.position, Quaternion.identity);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Instantiate(circle, transform.position, Quaternion.identity);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("EnemySquare") || other.CompareTag("EnemyCircle") || other.CompareTag("EnemyTriangle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ShapeShooter/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //TODO: Optimize this, can be way easier
+        if (other.CompareTag("EnemySquare") && gameObject.tag == "SquareProjectile")
+        {
+            Destroy(other.gameObject);
+            DestroyGameObject();
+        } else if (other.CompareTag("EnemyCircle") && gameObject.tag == "CircleProjectile")
+        {
+            Destroy(other.gameObject);
+            DestroyGameObject();
+        } else if (other.CompareTag("EnemyTriangle") && gameObject.tag == "TriangleProjectile")
+        {
+            Destroy(other.gameObject);
+            DestroyGameObject();
+        }
+    }
+
+    void DestroyGameObject(){
+        //Instantiate(effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
